4cb5ed770a1394576c0760a246ec3b1ad66ab6c0
Marius Gundersen <me@mariusgundersen.net>
2017-09-16 23:59:28 +0200

    Ready to deploy

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ let latest = null;
 
 async function fetch(){
   console.log('fetching', new Date());
-  const image = await Jimp.read("http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSGIODC_RGBNatColour_LowResolution.jpg");
-  const buffer = await new Promise((res, rej) => image.resize(360, 360).getBuffer('image/jpeg', (err, buffer) => err ? rej(err) : res(buffer)));
-  latest = buffer;
-  setTimeout(fetch, getTimeout());
+  try{
+    const image = await Jimp.read("http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSGIODC_RGBNatColour_LowResolution.jpg");
+    const buffer = await new Promise((res, rej) => image.resize(360, 360).getBuffer('image/jpeg', (err, buffer) => err ? rej(err) : res(buffer)));
+    latest = buffer;
+    console.log('fetch success');
+  }catch(e){
+    console.error(e.message)
+  }finally{
+    const milliseconds = getTimeout();
+    console.log(`next fetch in ${milliseconds/1000}s`)
+    setTimeout(fetch, milliseconds);
+  }
 }
 
 fetch();
@@ -19,6 +27,6 @@ http.createServer((req, res) => {
 }).listen(8080);
 
 const getTimeout = () => getMillisecondsUntilNextSecond() + 1000*(getSecondsUntilNextMinute() + 60*getMinutesUntilNextHour());
-const getMinutesUntilNextHour = () => 60 - new Date().getMinutes();
-const getSecondsUntilNextMinute = () => 60 - new Date().getSeconds();
+const getMinutesUntilNextHour = () => 59 - new Date().getMinutes();
+const getSecondsUntilNextMinute = () => 59 - new Date().getSeconds();
 const getMillisecondsUntilNextSecond = () => 1000 - new Date().getMilliseconds();
